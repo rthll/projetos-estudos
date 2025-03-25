e58fa11112f9018130514582d9fda6d35ca17e49
Rythielly Garcia <rthll@outlook.com>
2025-03-25 10:53:37 -0300

    uso de each com handlebars

---
+++ curso_NodeJs/15-repetição/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const exphbs = require('express-handlebars')
+const PORT = 3000;
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars');
+
+const users = {
+    "alunos": [
+        {
+            "name": "José",
+            "nota": 7
+        },
+        {
+            "name": "Thiago",
+            "nota": 9
+        },
+        {
+            "name": "Pedro",
+            "nota": 4
+        }
+    ]
+}
+
+const auth = true
+const approved = true
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Rythielly",
+        surname: "Francisco"
+    }
+
+    res.render('home', { user: user, auth, approved })
+})
+
+const items = [
+    "item A", "item B", "item C"
+]
+
+app.get('/dashboard', (req, res) => {
+    const user = {
+        name: "Rythielly",
+        surname: "Francisco"
+    }
+
+    res.render('dashboard.handlebars', { user: user, items: items } )
+})
+
+app.listen(PORT, () => {
+    console.log(`App rodando na porta ${PORT}`)
+});
