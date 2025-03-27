b9b6364b35cb46d5d0e8b3b2c55ff177d1a3a18b
Rythielly Garcia <rthll@outlook.com>
2025-03-27 09:27:18 -0300

    alterações no CSS das páginas

--- curso_NodeJs/20-manipulando_mysql/index.js
+++ curso_NodeJs/20-manipulando_mysql/index.js
@@ -4,13 +4,65 @@ const mysql = require('mysql2')
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine)
-app.set('view engine', 'handlebars')
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json()); // Para JSON
+app.use(express.urlencoded({ extended: true })); // Para formulário URL-encoded
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/books', (req,res) => {
+    const query = `SELECT * FROM books`
+    conn.query(query, function(err, data) {
+        if(err){
+            console.log(err)
+            return
+        }        
+
+        const books = data;
+        console.log(books)
+        res.render('books', { books })
+    })
+    
+})
+
+app.get('/book/:id', (req,res) => {
+    const id = req.params.id
+    const query = `SELECT * FROM books WHERE id = ${id}`
+    conn.query(query, function(err, data) {
+        if(err){
+            console.log(err)
+            return
+        }        
+
+        const book = data[0];
+        console.log(book)
+        res.render('book', { book })
+    })
+    
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    
+    const query = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`
+    conn.query(query, function(err) {
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/books')
+    })
 })
 
 const conn = mysql.createConnection({
